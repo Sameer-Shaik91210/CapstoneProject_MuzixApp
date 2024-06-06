b09184ad4045b7ed49c2f42efa4420776bb672b8
SHAIK SAMEER <shaiksameer773006@gmail.com>
2024-06-06 09:46:12 +0530

    Initialized Backend Project

---
+++ MuzixAppBackend/src/test/java/com/capstone/MuzixAppBackend/MuzixAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.MuzixAppBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuzixAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
