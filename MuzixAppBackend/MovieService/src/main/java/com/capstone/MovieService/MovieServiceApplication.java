24cf931d55de6372b719eb30f17a1419cfbcf604
SHAIK SAMEER <shaiksameer773006@gmail.com>
2024-06-10 10:11:00 +0530

    Created Movie Service with added functionality

--- MuzixAppBackend/MovieService/src/main/java/com/capstone/MovieService/MovieServiceApplication.java
+++ MuzixAppBackend/MovieService/src/main/java/com/capstone/MovieService/MovieServiceApplication.java
@@ -1,7 +1,10 @@
 package com.capstone.MovieService;
 
+import com.capstone.MovieService.filter.JwtFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MovieServiceApplication {
@@ -10,4 +13,11 @@ public class MovieServiceApplication {
 		SpringApplication.run(MovieServiceApplication.class, args);
 	}
 
+	@Bean
+	public FilterRegistrationBean jwtFilterBean(){
+		FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+		filterRegistrationBean.setFilter(new JwtFilter());
+		filterRegistrationBean.addUrlPatterns("/api/v1/user/*");
+		return filterRegistrationBean;
+	}
 }
--- MuzixAppBackend/MovieService/src/main/java/com/capstone/MovieService/controller/MovieServiceController.java
+++ MuzixAppBackend/MovieService/src/main/java/com/capstone/MovieService/controller/MovieServiceController.java
@@ -8,6 +8,7 @@ import com.capstone.MovieService.exception.MovieNotFoundEXception;
 import com.capstone.MovieService.exception.UserAlreadyExistsException;
 import com.capstone.MovieService.exception.UserNotFoundException;
 import com.capstone.MovieService.service.IMovieService;
+import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -38,25 +39,32 @@ public class MovieServiceController {
 
     //save favourite movie
 
-    @PostMapping("user/{userId}/movie")
-    public ResponseEntity<?> saveFavouriteMovie(@RequestBody FavouriteMovie movie, @PathVariable  String userId) throws UserNotFoundException, MovieAlreadyExistsException {
-  responseEntity=new ResponseEntity<User>(movieService.saveUserFavouriteMovieToTheMovieList(movie,userId),HttpStatus.OK);
+    @PostMapping("user/movie")
+    public ResponseEntity<?> saveFavouriteMovie(@RequestBody FavouriteMovie movie, HttpServletRequest request) throws UserNotFoundException, MovieAlreadyExistsException {
+    Claims claims=(Claims)request.getAttribute("claims");
+    String userId=claims.getSubject();
+
+      responseEntity=new ResponseEntity<User>(movieService.saveUserFavouriteMovieToTheMovieList(movie,userId),HttpStatus.OK);
         return responseEntity;
     }
 
     //delete a movie from user's favourite movie list
 
-    @DeleteMapping("user/{userId}/movie/{movieId}")
-    public ResponseEntity<?> deleteAMovieFromUserFavouriteMovieList(@PathVariable String movieId,@PathVariable String userId) throws UserNotFoundException, MovieNotFoundEXception {
-
+    @DeleteMapping("user/movie/{movieId}")
+    public ResponseEntity<?> deleteAMovieFromUserFavouriteMovieList(@PathVariable String movieId,HttpServletRequest request) throws UserNotFoundException, MovieNotFoundEXception {
+       Claims claims=(Claims)request.getAttribute("claims");
+       String userId=claims.getSubject();
         responseEntity=new ResponseEntity<User>(movieService.deleteAMovieFromUserFavouriteMovieList(movieId,userId),HttpStatus.OK);
         return responseEntity;
     }
 
     //retrieve all favourite movies
 
-    @GetMapping("user/movies/{userId}")
-    public ResponseEntity<?> getAllFavouriteMovies(@PathVariable String userId) throws Exception {
+    @GetMapping("user/movies")
+    public ResponseEntity<?> getAllFavouriteMovies(HttpServletRequest request) throws Exception {
+        Claims claims=(Claims)request.getAttribute("claims");
+        String userId=claims.getSubject();
+
        responseEntity=new ResponseEntity<List<FavouriteMovie>>(movieService.getAllFavouriteMoviesOfUser(userId),HttpStatus.OK);
         return responseEntity;
     }
